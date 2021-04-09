be6a3f8cc0a1d1eaa6e3463fc06e89aeeff6cdf8
Brian Graversen <bsg@digital-identity.dk>
2021-04-09 07:56:57 +0200

    increased max buffer size for websockets

--- backend/src/main/java/dk/digitalidentity/config/WebSocketConfig.java
+++ backend/src/main/java/dk/digitalidentity/config/WebSocketConfig.java
@@ -1,10 +1,12 @@
 package dk.digitalidentity.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 import dk.digitalidentity.service.SocketHandler;
 
@@ -17,5 +19,14 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
 		registry.addHandler(socketHandler, "/ws");
+	}
+	
+	@Bean
+	public ServletServerContainerFactoryBean createServletServerContainerFactoryBean() {
+	    ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+	    container.setMaxTextMessageBufferSize(512 * 1024);
+	    container.setMaxBinaryMessageBufferSize(512 * 1024);
+	    
+	    return container;
 	}
 }
